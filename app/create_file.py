11c49176c29a5a2676bd171094796a68aac2148a
Anna Kadlecova <adorosenko440@gmail.com>
2026-06-22 16:46:34 +0200

    test 4

--- app/create_file.py
+++ app/create_file.py
@@ -4,43 +4,48 @@ from datetime import datetime
 
 
 def write_input(path: str) -> None:
-    now_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     line = input("Enter content line: ")
-    number_of_line = 1
     if line == "stop":
         return
-    else:
-        with open(path, "a") as f:
-            if os.path.getsize(path) > 0:
-                f.write("\n")
-            f.write(now_str + "\n")
-            while not line == "stop":
-                f.write(f"{number_of_line} {line}\n")
-                line = input("Enter content line: ")
-                number_of_line += 1
+    number_of_line = 1
+    now_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S    ")
+    with open(path, "a") as f:
+        if os.path.getsize(path) > 0:
+            f.write("\n")
+        f.write(now_str + "\n")
+        while not line == "stop":
+            f.write(f"{number_of_line} {line}\n")
+            line = input("Enter content line: ")
+            number_of_line += 1
 
 
 def create_file() -> None:
-    if "-d" in sys.argv and "-f" in sys.argv:
-        f_position = sys.argv.index("-f")
-        d_position = sys.argv.index("-d")
-        file_txt = sys.argv[f_position + 1]
-        if d_position < f_position:
-            directory = sys.argv[(d_position + 1): f_position]
-        else:
-            directory = sys.argv[d_position + 1:]
+    args = sys.argv[1:]
+    if "-d" in args and "-f" in args:
+        directory = []
+        for index, arg in enumerate(args):
+            if arg == "-d":
+                dir_index = index + 1
+                while dir_index < len(args) and args[dir_index] != "-f":
+                    directory.append(args[dir_index])
+                    dir_index += 1
+        f_position = args.index("-f")
+        if f_position + 1 >= len(args):
+            print("No file specified")
+            return
+        file_txt = args[f_position + 1]
         directory_path = os.path.join(*directory)
         os.makedirs(directory_path, exist_ok=True)
         path = os.path.join(directory_path, file_txt)
         write_input(path)
         return
-    elif "-d" in sys.argv and "-f" not in sys.argv:
-        directory = sys.argv[sys.argv.index("-d") + 1:]
+    elif "-d" in args and "-f" not in args:
+        directory = args[args.index("-d") + 1:]
         directory_path = os.path.join(*directory)
         os.makedirs(directory_path, exist_ok=True)
         return
-    elif "-f" in sys.argv and "-d" not in sys.argv:
-        file_txt = sys.argv[sys.argv.index("-f") + 1]
+    elif "-f" in args and "-d" not in args:
+        file_txt = args[args.index("-f") + 1]
         write_input(file_txt)
         return
 
